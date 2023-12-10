81160d51765489dc15c67ee9df83789da1a787c7
KARIM BETTACHE <BETTACHEKARIM.BK@GMAIL.COM>
2023-12-10 17:27:07 +0100

    add clone & Comparable

--- Bank.java
+++ Bank.java
@@ -1,6 +1,6 @@
 import java.util.Objects;
 
-public class Bank {
+public class Bank implements Cloneable , Comparable<Bank> {
     private String name;
     private String location;
     private int numberOfBranches;
@@ -53,17 +53,39 @@ public class Bank {
                 Objects.equals(name, bank.name) &&
                 Objects.equals(location, bank.location);
     }
+    public Bank clone() {
+        try {
+            return (Bank) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This should not happen since Bank implements Cloneable
+            throw new InternalError(e);
+        }
+    }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, location, numberOfBranches);
     }
-
+    @Override
+    public int compareTo(Bank otherBank) {       
+        return Integer.compare(this.numberOfBranches, otherBank.numberOfBranches);      
+    }
+    
     public static void main(String[] args) {
         Bank bank1 = new Bank("ABC Bank", "City A", 10);
         Bank bank2 = new Bank("XYZ Bank", "City B", 15);
 
         System.out.println(bank1.toString());
         System.out.println("Are the banks equal? " + bank1.equals(bank2));
+        
+        int result = bank1.compareTo(bank2);
+
+        if (result < 0) 
+            System.out.println(bank1.getName() + " is less than " + bank2.getName());
+        if (result > 0) 
+            System.out.println(bank1.getName() + " is greater than " + bank2.getName());
+        if(result == 0)
+            System.out.println(bank1.getName() + " is equal to " + bank2.getName());
+        
     }
 }
